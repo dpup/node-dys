579d097ce8d389141531cfe6df452bac7614b435
pupius <dan.pupius@gmail.com>
2011-04-08 22:08:52 -0700

    MongoDB helpers

--- runtests.sh
+++ runtests.sh
@@ -5,4 +5,4 @@ if [ ! $nodeunit ]; then
   echo 'ERROR: No nodeunit found on path.'
   exit 1
 fi
-find . | grep _test.js | xargs $nodeunit
+find . | grep _test.js | grep -v ./node_modules/ | xargs $nodeunit
