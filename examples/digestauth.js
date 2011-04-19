1b4a47d398b4f7f90b3366b1037498ddd7187427
pupius <dan.pupius@gmail.com>
2011-04-19 15:14:54 -0700

    Basic implementation of digest auth

---
+++ examples/digestauth.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Example : Basic HTTP Auth
+ *!
+ * @author dan@pupi.us (Daniel Pupius)
+ */
+
+var dispatch = require('dispatch');
+
+new dispatch.Server(4108, '127.0.0.1').
+    addModule(new dispatch.RequestLogModule('/tmp')).
+    addModule(new dispatch.StatsModule()).
+    addModule(new dispatch.auth.DigestAuthModule('Digest Test Realm').
+        // Provide a hardcoded set of usernam/password pairs.
+        withUserMap({
+          'dan': 'dan123',
+          'bob': 'bob456',
+          'bill': 'bill789'
+        }).
+        // Only require auth for paths beginning with 'a'.
+        useRegExpFilter(/\/a.*/)).
+    // Add an action that will match any path.
+    addAction('/*', function(ctx) {      
+      var req = ctx.get('request');
+      var res = ctx.get('response');
+      var user = ctx.get('auth.user');
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Request Url : ' + req.url + '\n' +
+              'User : ' + (user || 'N/A'));
+    }).
+    start();
